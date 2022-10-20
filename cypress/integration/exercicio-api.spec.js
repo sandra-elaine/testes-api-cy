2cffff58179c23f7368fe6194208c0c6279e5714
Sandra <sandraelainedeoliveira@gmail.com>
2022-10-20 12:42:31 -0300

    primeiro commit

--- cypress/integration/exercicio-api.spec.js
+++ cypress/integration/exercicio-api.spec.js
@@ -1,30 +1,91 @@
 /// <reference types="cypress" />
+import contrato from '../contracts/usuarios.contract'
+var faker = require('faker');
 
 describe('Testes da Funcionalidade Usuários', () => {
 
-    it('Deve validar contrato de usuários', () => {
-         //TODO: 
-    });
+     let token
+     before(() => {
+          cy.token('fulano@qa.com', 'teste').then(tkn => { token = tkn })
+     });
 
-    it('Deve listar usuários cadastrados', () => {
-         //TODO: 
-    });
+     it('Deve validar contrato de usuários', () => {
+          //TODO: 
+          cy.request('usuarios').then(response => {
+               return contrato.validateAsync(response.body)
+          })
+     });
 
-    it('Deve cadastrar um usuário com sucesso', () => {
-         //TODO: 
-    });
+     it('Deve listar usuários cadastrados', () => {
+          //TODO:
+          cy.request({
+               method: 'GET',
+               url: 'usuarios'
+          }).then((response) => {
+               expect(response.status).to.equal(200)
+               expect(response.body).to.have.property('usuarios')
+               expect(response.duration).to.be.lessThan(25)
+          })
+     });
 
-    it('Deve validar um usuário com email inválido', () => {
-         //TODO: 
-    });
+     it('Deve cadastrar um usuário com sucesso', () => {
+          //TODO:
+          let email = faker.internet.email()
+          cy.cadastrarUsuario("Fulano da Silva", email, "teste", "true")
+               .then((response) => {
+                    expect(response.status).to.equal(201)
+                    expect(response.body.message).to.equal('Cadastro realizado com sucesso')
+                    expect(response.duration).to.be.lessThan(15)
+               })
+     });
 
-    it('Deve editar um usuário previamente cadastrado', () => {
-         //TODO: 
-    });
+     it('Deve validar um usuário com email inválido', () => {
+          //TODO:
+          cy.cadastrarUsuario('Fulano da Silva', 'fulano@qa.com', 'teste', 'true')
+               .then((response) => {
+                    expect(response.status).to.equal(400)
+                    expect(response.duration).to.be.lessThan(10)
+               })
+     });
 
-    it('Deve deletar um usuário previamente cadastrado', () => {
-        //TODO: 
-    });
+     it('Deve editar um usuário previamente cadastrado', () => {
+          //TODO:
+          let email = faker.internet.email()
+          cy.cadastrarUsuario("Fulano da Silva", email, "teste", "true")
+               .then(response => {
+                    let id = response.body._id
+                    cy.request({
+                         method: 'PUT',
+                         url: `usuarios/${id}`,
+                         headers: { authorization: token },
+                         body:
+                         {
+                              "nome": "Fulano Marcos da Silva",
+                              "email": email,
+                              "password": "teste",
+                              "administrador": "true"
+                         }
+                    }).then(response => {
+                         expect(response.status).to.equal(200)
+                         expect(response.body.message).to.equal('Registro alterado com sucesso')
+                         expect(response.duration).to.be.lessThan(12)
+                    })
+               })
+     });
 
-
+     it('Deve deletar um usuário previamente cadastrado', () => {
+          //TODO:
+          let email = faker.internet.email()
+          cy.cadastrarUsuario("Fulano da Silva", email, "teste", "true")
+               .then(response => {
+                    let id = response.body._id
+                    cy.request({
+                         method: 'DELETE',
+                         url: `usuarios/${id}`,
+                         headers: { authorization: token }
+                    }).then(response => {
+                         expect(response.body.message).to.equal('Registro excluído com sucesso')
+                    })
+               })
+     });
 });
